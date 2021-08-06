8ba246c7a22d820d54e3d8d6e7fe86d6dc7c4f95
Personal Robot <86073822+Agoriadmin@users.noreply.github.com>
2021-08-06 20:32:26 +0530

    Update and rename uploader.py to uploader.py

--- helpers/uploader.py
+++ helpers/uploader.py
@@ -45,9 +45,9 @@ async def UploadFile(bot: Client, message: Message, file_path: str, file_size):
             caption=((Config.CAPTION.format((await bot.get_me()).username) + f"\n\n**File Name:** `{file_path.rsplit('/', 1)[-1]}`\n**File Size:** `{humanbytes(file_size)}`") if (caption_ is None) else caption_),
             reply_markup=InlineKeyboardMarkup(
                 [
-                    [InlineKeyboardButton("Developer - @AbirHasan2005", url="https://t.me/AbirHasan2005")],
-                    [InlineKeyboardButton("Support Group", url="https://t.me/DevsZone"),
-                     InlineKeyboardButton("Bots Channel", url="https://t.me/Discovery_Updates")]
+                    [InlineKeyboardButton("Developer - @personal Robot", url="https://t.me/personal_privetbot1")],
+                    [InlineKeyboardButton("Support Group", url="https://t.me/agorimovies"),
+                     InlineKeyboardButton("Bots Channel", url="https://t.me/agorihome")]
                 ]
             )
         )
